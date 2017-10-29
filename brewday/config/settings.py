62d0eb6ce367ead5cd1060cfb5ee4cebc0045da9
Victor Martinez <vcrmartinez@gmail.com>
2017-10-29 19:16:19 -0300

    Allow user to login using username or email

--- brewday/config/settings.py
+++ brewday/config/settings.py
@@ -6,7 +6,6 @@ from decouple import config, Csv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -17,7 +16,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
-
 
 # Application definition
 
@@ -68,7 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -76,7 +73,6 @@ DEFAULT_DBURL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 DATABASES = {
     'default': config('DATABASE_URL', default=DEFAULT_DBURL, cast=dburl)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -96,6 +92,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'src.accounts.backends.LoginBackend',
+)
+
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = 'core:index'
 LOGOUT_REDIRECT_URL = 'core:login'
@@ -113,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
---
+++ brewday/src/accounts/backends.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.backends import ModelBackend as BaseModelBackend
+
+from .models import User
+
+
+class LoginBackend(BaseModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is not None:
+            try:
+                user = User.objects.get(email=username)
+                if user.check_password(password):
+                    return user
+            except User.DoesNotExist:
+                pass
