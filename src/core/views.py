111a19871bf97ba8d6c7b225bf19e1deac0942ca
Victor Martinez <vcrmartinez@gmail.com>
2017-11-04 22:18:10 -0300

    Implement app view. Redirect user to app if logged

--- src/core/views.py
+++ src/core/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return HttpResponseRedirect(reverse_lazy('core:app'))
+        return super(IndexView, self).get(request, *args, **kwargs)
 
 
 class AppView(LoginRequiredMixin, TemplateView):
