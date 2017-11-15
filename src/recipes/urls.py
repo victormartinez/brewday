21c231241a95e186d9b4dbacd9491d977ef6ca90
Victor Martinez <vcrmartinez@gmail.com>
2017-11-14 23:42:24 -0300

    Add my recipes view. Update nav

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from .views import new_recipe
+from .views import new_recipe, my_recipes
 
 urlpatterns = [
+    url(r'^my-recipes$', my_recipes, name='my_recipes'),
     url(r'^new$', new_recipe, name='new'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import TemplateView
 
 
+class MyRecipesView(TemplateView):
+    template_name = 'recipes/my.html'
+
+
 class NewRecipeView(TemplateView):
     template_name = 'recipes/new.html'
 
 
 new_recipe = NewRecipeView.as_view()
+my_recipes = MyRecipesView.as_view()
