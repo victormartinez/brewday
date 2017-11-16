e9102189df10dda1277d9c9ee6fefb8d60d69c3d
Victor Martinez <vcrmartinez@gmail.com>
2017-11-16 19:49:42 -0300

    Implement basic view for storage

--- src/storages/views.py
+++ src/storages/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MyStorageView(TemplateView):
+    template_name = 'storages/my.html'
+
+
+class NewIngredientView(TemplateView):
+    template_name = 'storages/new.html'
+
+
+my_storage = MyStorageView.as_view()
+new_ingredient = NewIngredientView.as_view()
