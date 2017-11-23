469bc53308f6afb2512901444b16776354a2fbe2
Victor Martinez <vcrmartinez@gmail.com>
2017-11-22 21:20:11 -0300

    Create recipe and ingredient models. Make migration

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from model_utils.models import TimeStampedModel
+
+
+class Unit(TimeStampedModel):
+    KILOGRAM = 'kg'
+    GRAM = 'g'
+    MILIGRAM = 'mg'
+    LITRE = 'l'
+    MILILITRE = 'ml'
+
+    UNIT_CHOICES = (
+        (MILIGRAM, 'mg'),
+        (GRAM, 'gram'),
+        (KILOGRAM, 'kilogram'),
+        (LITRE, 'litre'),
+        (MILILITRE, 'ml'),
+    )
+
+    name = models.CharField(max_length=2, choices=UNIT_CHOICES)
+
+
+class Recipe(TimeStampedModel):
+    title = models.CharField('Title', max_length=255, help_text='Title of the recipe.')
+    og = models.DecimalField('OG', max_digits=4, decimal_places=4, blank=True, null=True, help_text='Original Gravity')
+    fg = models.DecimalField('FG', max_digits=4, decimal_places=4, blank=True, null=True, help_text='Final Gravity')
+    ibu = models.PositiveIntegerField('IBU', blank=True, null=True, help_text='International Bitterness Unit')
+    srm = models.PositiveIntegerField('SRV', blank=True, null=True, help_text='Standard Reference Method')
+    abv = models.DecimalField('ABV', max_digits=2, decimal_places=2, blank=True, null=True, help_text='Alcohool by Volume')
+
+    steps = models.TextField('Steps')
+    observations = models.TextField('Observations', blank=True, null=True)
+
+
+class Ingredient(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    quantity = models.PositiveIntegerField()
+    unit = models.ForeignKey(Unit)
+    recipe = models.ForeignKey(Recipe)
