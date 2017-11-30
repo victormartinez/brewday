c493360387e8de93bc914f5e38eb89894d5fd5fc
Victor Martinez <vcrmartinez@gmail.com>
2017-11-30 18:48:48 -0300

    Create equipment models and make migrations

--- src/equipments/models.py
+++ src/equipments/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from src.units.models import UNIT_CHOICES
+
+User = get_user_model()
+
+
+class Equipment(TimeStampedModel):
+    """
+    AIRLOCK
+    BOILING_POT
+    BOTTLE
+    BOTTLE_CAPPER
+    BOOTLE_CAPS
+    BOOTLE_BRUSH
+    FERMENTER
+    MEASURING_CUP
+    SIPHON
+    RACKING_CANE
+    BOTTLE_FILLER
+    STIRRING_PADDLE
+    THERMOMETER
+    BOTTLING_BUCKET
+    HYDROMETER
+    WINE_THIEF
+    HYDROMETER_JAR
+    WORT_CHILLER
+    STRAINER
+    """
+
+    name = models.CharField(unique=True, max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+
+class UserEquipment(TimeStampedModel):
+    # TODO: Later on will have a link to batch indicating that is not available.
+
+    user = models.ForeignKey(User)
+    equipment = models.ForeignKey(Equipment)
+    capacity = models.DecimalField('Qty', max_digits=8, decimal_places=4)
+    unit = models.CharField(max_length=2, choices=UNIT_CHOICES)
