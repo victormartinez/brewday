7156ecac88e485c1da7d0e43b32b9ee9b3ad6c58
Victor Martinez <vcrmartinez@gmail.com>
2017-12-04 21:33:48 -0300

    Add equipment models to admin

--- src/equipments/admin.py
+++ src/equipments/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from src.equipments.models import Equipment
+from src.equipments.models import Equipment, UserEquipment, RecipeEquipment
 
 admin.site.register(Equipment)
+admin.site.register(UserEquipment)
+admin.site.register(RecipeEquipment)
