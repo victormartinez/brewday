be23fd2237af09f59371f74e2459cb4294fd8f34
Victor Martinez <vcrmartinez@gmail.com>
2017-12-07 22:50:48 -0300

    Refactor Ingredient model to use measurement

--- src/ingredients/models.py
+++ src/ingredients/models.py
@@ -1,18 +1,33 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+from django_measurement.models import MeasurementField
 from model_utils.models import TimeStampedModel
+from measurement.measures import Volume, Weight
 
 from src.recipes.models import Recipe
-from src.units.models import UNIT_CHOICES
 
 User = get_user_model()
 
 
 class Ingredient(TimeStampedModel):
+    GRAIN = 'grain'
+    HOP = 'hop'
+    YEAST = 'yeast'
+    WATER = 'water'
+    ADDITIVE = 'additive'
+    INGREDIENT_TYPE_CHOICES = (
+        (GRAIN, 'Grain'),
+        (HOP, 'Hop'),
+        (YEAST, 'Yeast'),
+        (WATER, 'Water'),
+        (ADDITIVE, 'Additive'),
+    )
+
     name = models.CharField(max_length=255)
-    quantity = models.DecimalField('Qty', max_digits=8, decimal_places=4)
-    unit = models.CharField(max_length=2, choices=UNIT_CHOICES)
+    ingredient_type = models.CharField(choices=INGREDIENT_TYPE_CHOICES, max_length=10)
+    volume_quantity = MeasurementField(measurement=Volume, blank=True, null=True)
+    weight_quantity = MeasurementField(measurement=Weight, blank=True, null=True)
 
     class Meta:
         abstract = True
@@ -22,7 +37,9 @@ class RecipeIngredient(Ingredient):
     recipe = models.ForeignKey(Recipe)
 
     def __str__(self):
-        return "{}: {}{}".format(self.name, self.quantity, self.unit)
+        if self.volume_quantity:
+            return "{}: {}".format(self.name, self.volume_quantity)
+        return "{}: {}".format(self.name, self.weight_quantity)
 
 
 class UserIngredient(Ingredient):
