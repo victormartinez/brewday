0b923af1fd177974c7aaee09d987e476e8dff7c1
Victor Martinez <vcrmartinez@gmail.com>
2018-01-08 23:32:34 -0300

    Allow user to restart a batch

--- src/batches/urls.py
+++ src/batches/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import my_batches, finish_batch
+from .views import my_batches, finish_batch, restart_batch
 
 urlpatterns = [
     url(r'^$', my_batches, name='my'),
     url(r'(?P<pk>[\w-]+)/finish$', finish_batch, name='finish'),
+    url(r'(?P<pk>[\w-]+)/restart', restart_batch, name='restart'),
 ]
--- src/batches/views.py
+++ src/batches/views.py
@@ -7,6 +7,28 @@ from django.views.generic import ListView, UpdateView
 from src.batches.models import RecipeBatch
 
 
+class MyBatchesView(LoginRequiredMixin, ListView):
+    template_name = 'batches/my.html'
+
+    def get_queryset(self):
+        return RecipeBatch.objects.filter(user=self.request.user).order_by('created')
+
+
+class RestartBatchView(LoginRequiredMixin, UpdateView):
+    model = RecipeBatch
+    fields = ('restarted', 'finished',)
+
+    def post(self, request, *args, **kwargs):
+        request.POST = request.POST.copy()
+        request.POST['restarted'] = datetime.now()
+        request.POST['finished'] = None
+        return super().post(request, *args, **kwargs)
+
+    def get_success_url(self):
+        messages.success(self.request, 'You have restarted a batch.')
+        return reverse('batches:my')
+
+
 class FinishBatchView(LoginRequiredMixin, UpdateView):
     model = RecipeBatch
     fields = ('finished',)
@@ -21,12 +43,6 @@ class FinishBatchView(LoginRequiredMixin, UpdateView):
         return reverse('batches:my')
 
 
-class MyBatchesView(LoginRequiredMixin, ListView):
-    template_name = 'batches/my.html'
-
-    def get_queryset(self):
-        return RecipeBatch.objects.filter(user=self.request.user).order_by('created')
-
-
 my_batches = MyBatchesView.as_view()
 finish_batch = FinishBatchView.as_view()
+restart_batch = RestartBatchView.as_view()
